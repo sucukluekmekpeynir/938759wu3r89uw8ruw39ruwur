80ebe1cccc8638fe80d0046dbdd6d2ecd9f52d8a
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2018-10-19 19:05:32 +0300

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -6,10 +6,29 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Discord: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Discord: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Discord: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setGame(`${prefix}yardım Kanalımız İçin //link yazabilirsin. Made İn Türkiye!!!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-};
+  client.user.setGame("Şu an 4K adet Kanala 2K sunucuya 5K kullanıcıya hizmet veriliyor")
+client.user.setUsername(" Kralbot ");
+client.user.setPresence({
+        game: {
+            name: `Patates izliyor`,
+            type: 'WATCHING'
+        },
+        status: 'dnd'
+    })
+var oyun = [
+        "//yardım",
+        "Kralbot Sürümü 1.0.0.0 Olarak Güncellenmiştir!",
+        "Şu an 4K adet Kanala 2K sunucuya 5K kullanıcıya hizmet veriliyor"             
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/SuloTR%22");
+        }, 2 * 2500);
+}
